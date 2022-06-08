5cd743d69cdacefae6d0585a1797d4c9b3a74990
nOw Innovation Inc <57865296+nowinnovation@users.noreply.github.com>
2022-06-08 15:43:43 -0230

    Refactoring to eliminate unnecessary dependencies for BSDL parsing

---
+++ lib_jtag_core/src/bsdl_parser/jtag_bsdl.h
@@ -0,0 +1,92 @@
+/*
+ * JTAG Core library
+ * Copyright (c) 2008 - 2021 Viveris Technologies
+ *
+ * JTAG Core library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * JTAG Core library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JTAG Core library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * @file   jtag_bsdl.h
+ * @brief  bsdl file parser header with minimal dependencies
+ * @author Jean-François DEL NERO <Jean-Francois.DELNERO@viveris.fr>
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_ELEMENT_SIZE (64+1)
+
+typedef struct _pin_ctrl
+{
+	char pinname[MAX_ELEMENT_SIZE];
+	int  pintype;
+
+	char physical_pin[MAX_ELEMENT_SIZE];
+
+	int ctrl_bit_number;
+	int out_bit_number;
+	int in_bit_number;
+}pin_ctrl;
+
+typedef struct _jtag_chain
+{
+	int bit_index;
+
+	int bit_cell_type;                // BC_1,BC_2,...
+
+	char pinname[MAX_ELEMENT_SIZE];   // Pin name.
+
+	int bit_type;                     // None , ctrl , in, out.
+
+	int safe_state;                   // Default - Safe state. (0,1,-1)
+
+	int control_bit_index; // Indicate the associated control bit. -1 if no control bit.
+	int control_disable_state;
+	int control_disable_result;
+
+}jtag_chain;
+
+typedef struct _jtag_bsdl
+{
+	unsigned long chip_id;
+
+	char src_filename[512];
+	char entity_name[512];
+
+	int number_of_chainbits;
+	jtag_chain * chain_list;
+
+	int number_of_pins;
+	pin_ctrl * pins_list;
+
+	int number_of_bits_per_instruction;
+	char IDCODE_Instruction[MAX_ELEMENT_SIZE];
+	char EXTEST_Instruction[MAX_ELEMENT_SIZE];
+	char BYPASS_Instruction[MAX_ELEMENT_SIZE];
+	char SAMPLE_Instruction[MAX_ELEMENT_SIZE];
+
+}jtag_bsdl;
+
+#ifndef _JTAGCORE_PRINT_FUNC_
+typedef void (*JTAGCORE_PRINT_FUNC)(char * string);
+#define _JTAGCORE_PRINT_FUNC_
+#endif
+
+jtag_bsdl * jtag_bsdl_load_file(JTAGCORE_PRINT_FUNC print_callback, int logs_level, int sort_pins, char *filename);
+void jtag_bsdl_unload_file(jtag_bsdl * bsdl);
+
+#ifdef __cplusplus
+}
+#endif

1fc2fabfaa5340b427e6f379a168e132979dc791
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2023-04-11 20:05:46 +0200

    CI update : should now build the software and prepare the release zip file.

---
+++ build/windowscreatezip.sh
@@ -0,0 +1,11 @@
+mkdir JTAGBoundaryScanner
+mkdir JTAGBoundaryScanner/Windows$1
+cp ../hxcfloppyemulator_soft_release_notes.txt ./JTAGBoundaryScanner/ || exit 1
+cp ../LICENSE ./JTAGBoundaryScanner/ || exit 1
+cp ../README.md ./JTAGBoundaryScanner/ || exit 1
+cp ../Release-notes.txt ./JTAGBoundaryScanner/ || exit 1
+cp *.dll ./JTAGBoundaryScanner/Windows$1/ || exit 1
+cp *.exe ./JTAGBoundaryScanner/Windows$1/ || exit 1
+cp ../lib_jtag_core/src/config.script ./JTAGBoundaryScanner/Windows$1/config.script || exit 1
+
+zip -r JTAGBoundaryScanner_win$1.zip JTAGBoundaryScanner/

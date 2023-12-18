77541a0c82a3ba8630fce1e571bc69e554f824ed
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2023-12-18 19:57:19 +0100

    lib and app common version definitions.

--- lib_jtag_core/src/version.h
+++ lib_jtag_core/src/version.h
@@ -1,13 +1,16 @@
-#define VDIG1 0
-#define VDIG2 9
-#define VDIG3 6
+#define VDIG1 2
+#define VDIG2 6
+#define VDIG3 3
 #define VDIG4 1
 
-#define STR_DATE "20 Aug 2023"
+#define STR_DATE "18 Dec 2023"
 
 #define vxstr(s) vstr(s)
 #define vstr(s) #s
 
 #define LIB_JTAG_CORE_VERSION  vxstr(VDIG1) "." vxstr(VDIG2) "." vxstr(VDIG3) "." vxstr(VDIG4)
+#define LIB_JTAG_CORE_VERSION_COMMA  vxstr(VDIG1) "," vxstr(VDIG2) "," vxstr(VDIG3) "," vxstr(VDIG4)
 
-
+#define APP_VER          VDIG1.VDIG2.VDIG3.VDIG4
+#define APP_VER_TXT( N ) JTAG Boundary Scanner v##N
+#define APP_VER_STR( N ) vxstr( APP_VER_TXT( N ) )

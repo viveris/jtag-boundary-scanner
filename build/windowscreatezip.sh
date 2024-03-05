35d37fd9096a1884eb3fad4ab1ddfc78655e057e
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2024-03-05 22:34:52 +0100

    zip creation : add bsdl_files folder.

--- build/windowscreatezip.sh
+++ build/windowscreatezip.sh
@@ -6,5 +6,6 @@ cp ../Release-notes.txt ./JTAGBoundaryScanner/ || exit 1
 cp *.dll ./JTAGBoundaryScanner/Windows$1/ || exit 1
 cp *.exe ./JTAGBoundaryScanner/Windows$1/ || exit 1
 cp ../lib_jtag_core/src/config.script ./JTAGBoundaryScanner/Windows$1/config.script || exit 1
+mkdir JTAGBoundaryScanner/Windows$1/bsdl_files
 
 zip -r JTAGBoundaryScanner_win$1.zip JTAGBoundaryScanner/

4feb826493495d4fd7c18e5ce6dd6c626374eafb
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2024-10-28 20:22:58 +0100

    v2.6.7.1

--- lib_jtag_core/src/version.h
+++ lib_jtag_core/src/version.h
@@ -1,9 +1,9 @@
 #define VDIG1 2
 #define VDIG2 6
-#define VDIG3 6
+#define VDIG3 7
 #define VDIG4 1
 
-#define STR_DATE "16 Oct 2024"
+#define STR_DATE "28 Oct 2024"
 
 #define vxstr(s) vstr(s)
 #define vstr(s) #s
